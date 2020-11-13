a1ff0d72c396d2bd3cd7633040f55a71eccfb1e0
Craig Scrimgeour <craigscrimgeour@craigs-mbp.home>
2020-11-13 10:46:17 +0000

    created basic components in each file

---
+++ src/components/PasswordForget/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PasswordChange from '../PasswordChange';
+ 
+const PasswordForget = () => (
+  <div>
+    <h1>App</h1>
+  </div>
+);
+ 
+export default PasswordChange;
